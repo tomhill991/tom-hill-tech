8e2e871077a9c0f92ab9d8af8948c0c5f69a58ea
Tom Hill <tom.hill991@gmail.com>
2020-01-14 22:12:53 +0800

    landing page completed

---
+++ src/components/main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import LandingPage from './landingpage';
+import AboutMe from './aboutme';
+import Contact from './contact';
+import Projects from './projects';
+import Resume from './resume';
+import { Switch, Route } from 'react-router-dom';
+
+const Main = () => (
+  <Switch>
+    <Route exact path="/" component={LandingPage} />
+    <Route path="/aboutme" component={AboutMe} />
+    <Route path="/contact" component={Contact} />
+    <Route path="/projects" component={Projects} />
+    <Route path="/resume" component={Resume} />
+  </Switch>
+)
+
+export default Main;
